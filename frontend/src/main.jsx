09d36ea05585af9130fd1e289cdaf030a16e6c08
rop2024 <rohitpandey231456@gmail.com>
2025-12-12 11:58:00 +0530

    Fix routing bug - Remove nested Routes from main.jsx and let App.jsx handle all routing - This resolves 'No routes matched location /login' error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import RecipeDetail from './components/RecipeDetail.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
